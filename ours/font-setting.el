f58ce04e7e035f1e59bb7abacf94150078403d65
MACAir <harrifeng@gmail.com>
2012-12-22 21:03:12 +0800

    use monaco and 14 font default for mac

--- ours/font-setting.el
+++ ours/font-setting.el
@@ -2,7 +2,7 @@
 
 (defconst my-font-size (cond
 			((string-match system-name "hfeng.local")
-			 ":pixelsize=16")
+			 ":pixelsize=14")
 			((string-match system-name "hfeng-desktop")
 			 ":pixelsize=20")
 			((string-match system-name "hfeng-pc")
@@ -12,5 +12,5 @@
 			(":pixelsize=16")))
 
 (qiang-set-font
- '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") my-font-size
+ '("Monaco" "Consolas" "DejaVu Sans Mono" "Monospace" "Courier New") my-font-size
  '("楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" ))
