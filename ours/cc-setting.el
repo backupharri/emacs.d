ba5f3c47510fb517df6f1e791348225ebcea371a
T60p <harrifeng@gmail.coM>
2012-12-24 21:53:27 +0800

    new default style

--- ours/cc-setting.el
+++ ours/cc-setting.el
@@ -1,4 +1,11 @@
 (provide 'cc-setting)
 
-(setq c-default-style "stroustrup"
+;; (setq c-default-style "stroustrup"
+;;       c-basic-offset 4)
+(setq c-default-style
+      (quote ((c-mode . "bsd")
+	      (c++-mode . "stroustrup")
+	      (java-mode . "java")
+	      (awk-mode . "awk")))
       c-basic-offset 4)
+
