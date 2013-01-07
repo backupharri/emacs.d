f66b99a48317d2b0862785b45fa28ab942f9346b
sh-rd-hfeng <haoran.feng@ni.com>
2013-01-07 15:08:38 +0800

    update bat file mode

--- ours/simpleplugin-setting.el
+++ ours/simpleplugin-setting.el
@@ -4,3 +4,15 @@
 
 (require 'htmlize)
 (require 'browse-kill-ring)
+
+;; bat-mode setting
+(setq auto-mode-alist 
+      (append 
+       (list (cons "\\.[bB][aA][tT]$" 'bat-mode))
+       ;; For DOS init files
+       (list (cons "CONFIG\\."   'bat-mode))
+       (list (cons "AUTOEXEC\\." 'bat-mode))
+       auto-mode-alist))
+
+(autoload 'bat-mode "bat-mode"
+  "DOS and WIndows BAT files" t)
