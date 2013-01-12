0910f4c58c56d607972e7ad6d8ba3d9551f890ca
MacAir <harrifeng@gmail.com>
2013-01-12 12:14:08 +0800

    solve font problem, only useful in mac

--- ours/basic-setting.el
+++ ours/basic-setting.el
@@ -64,6 +64,19 @@
 ;; (prefer-coding-system 'utf-16)
 ;; (prefer-coding-system 'utf-8)
 
+;;UTF-8 Setting
+(set-language-environment 'Chinese-GB)
+(setq-default pathname-coding-system 'euc-cn)
+(setq file-name-coding-system 'euc-cn)
+
+;;Encoding setting
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+(prefer-coding-system 'gb18030)
+(prefer-coding-system 'utf-16)
+(prefer-coding-system 'utf-8)
+
 ;;auto expand
 (setq hippie-expand-try-functions-list
       '(
