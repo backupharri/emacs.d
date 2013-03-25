a75216545df5cf38885e6f3c5ba160ae7dd15211
sh-linux-hfeng <harrifeng@gmail.com>
2013-03-25 19:35:50 +0800

    for linux

--- ours/colortheme-setting.el
+++ ours/colortheme-setting.el
@@ -3,5 +3,4 @@
 (add-to-list 'custom-theme-load-path (concat their-lisps-path
 					     "colortheme"))
 
-;; (load-theme 'solarized-dark t)
-(load-theme 'tomorrow-night-eighties t)
+(load-theme 'tomorrow t)
--- ours/font-setting.el
+++ ours/font-setting.el
@@ -3,15 +3,15 @@
 (defconst my-font-size (cond
 			((string-match system-name "hfengs-MacBook-Air.local")
 			 ":pixelsize=14")
-			((string-match system-name "hfeng-desktop")
-			 ":pixelsize=20")
+			((string-match system-name "sh-linux-hfeng")
+			 ":pixelsize=18")
 			((string-match system-name "hfeng-pc")
 			 ":pixelsize=20")
 			((string-match system-name "sh-rd-hfeng")
 			 ":pixelsize=14")
-			(":pixelsize=16")))
+			(":pixelsize=14")))
 
 (qiang-set-font
- '("Monaco" "Consolas" "Courier" "DejaVu Sans Mono" "Monospace" "Courier New") my-font-size
+ '("Monaco" "Consolas" "Courier New" "DejaVu Sans Mono" "Monospace" "Courier") my-font-size
  '("楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" ))
 
