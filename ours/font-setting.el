65405d336e999012b256f49548e7adb5301a5864
t60p <harrifeng@gmail.com>
2013-04-18 22:12:01 +0800

    for thinkpad modify font

--- ours/font-setting.el
+++ ours/font-setting.el
@@ -5,13 +5,13 @@
 			 ":pixelsize=14")
 			((string-match system-name "hfeng-desktop")
 			 ":pixelsize=20")
-			((string-match system-name "hfenghome")
+			((string-match system-name "hfeng-t60p")
 			 ":pixelsize=14")
 			((string-match system-name "sh-rd-hfeng")
 			 ":pixelsize=14")
 			(":pixelsize=16")))
 
 (qiang-set-font
- '("Monaco" "Consolas" "Courier" "DejaVu Sans Mono" "Monospace" "Courier New") my-font-size
+ '("Consolas" "Monaco" "Courier" "DejaVu Sans Mono" "Monospace" "Courier New") my-font-size
  '("楷体" "kai" "Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" ))
 
